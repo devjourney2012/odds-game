82a88a0e9d56d77b7727624edb5369df63f81675
chris <chris.arvidsson1@gmail.com>
2024-11-06 04:11:08 -0500

    Create initial project

---
+++ src/main/kotlin/com/example/odds_game/OddsGameApplication.kt
@@ -0,0 +1,11 @@
+package com.example.odds_game
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OddsGameApplication
+
+fun main(args: Array<String>) {
+	runApplication<OddsGameApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/odds_game/OddsGameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.odds_game
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OddsGameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
